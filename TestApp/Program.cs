6386baae8c78ce2743e84e2950ed0144fe0af4c5
felix <xilebou@tutanota.com>
2026-03-09 18:54:50 -0400

    Add test project

---
+++ TestApp/Program.cs
@@ -0,0 +1,30 @@
+using AspNetFormFramework.FormGeneration;
+using TestApp.Controller;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseRouting();
+
+app.UseForms<TestController>();
+
+app.MapControllers();
+
+app.MapDefaultControllerRoute();
+app.Run();
+
+
+static class ApplicationFormExtension
+{
+    public static void UseForms<T>(this WebApplication app) where T : IFormController
+    {
+        FormMapper<T> mapper = new FormMapper<T>(app);
+        mapper.MapForms();
+    }
+}
